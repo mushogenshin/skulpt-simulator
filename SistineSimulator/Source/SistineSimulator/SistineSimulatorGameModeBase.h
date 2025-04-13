39a5bf65dccb2fff0dac25b088e835902006df72
Hoan Nguyen <info@mushogenshin.com>
2025-04-13 16:56:45 +0700

    Unreal 5.5 C++/Scalable project

---
+++ SistineSimulator/Source/SistineSimulator/SistineSimulatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SistineSimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SISTINESIMULATOR_API ASistineSimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
