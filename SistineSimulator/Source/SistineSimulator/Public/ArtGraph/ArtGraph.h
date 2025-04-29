7746587d0ce889288f62c0ebf44cc3efa6184551
Hoan Nguyen <info@mushogenshin.com>
2025-04-29 09:30:41 +0700

    WIP: GameplayTag to glue DataAssets and Actors

--- SistineSimulator/Source/SistineSimulator/Public/ArtGraph/ArtGraph.h
+++ SistineSimulator/Source/SistineSimulator/Public/ArtGraph/ArtGraph.h
@@ -1,21 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
 #include "ArtGraph.generated.h"
 
 class UGraphElement;
-
-USTRUCT(BlueprintType)
-struct SISTINESIMULATOR_API FNodeTransform
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ArtGraph")
-	FVector Position;
-};
 
 USTRUCT(BlueprintType)
 struct SISTINESIMULATOR_API FGraphEdge
@@ -23,10 +13,10 @@ struct SISTINESIMULATOR_API FGraphEdge
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ArtGraph")
-	TObjectPtr<UGraphElement> NodeA;
+	TObjectPtr<UGraphElement> ElementA;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ArtGraph")
-	TObjectPtr<UGraphElement> NodeB;
+	TObjectPtr<UGraphElement> ElementB;
 };
 
 /**
@@ -38,9 +28,10 @@ class SISTINESIMULATOR_API UGraphElement : public UDataAsset
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Transform")
-	FVector Position;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tag")
+	FGameplayTag Tag;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Connections")
 	TArray<FGraphEdge> AdjacencyList;
 };
+
