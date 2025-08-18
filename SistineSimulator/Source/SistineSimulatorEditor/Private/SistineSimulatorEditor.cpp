006aa834dcd46b1d95f00f3a02bbc0be80672cef
Hoan Nguyen <info@mushogenshin.com>
2025-08-18 23:04:31 +0700

    WIP: Editor module

---
+++ SistineSimulator/Source/SistineSimulatorEditor/Private/SistineSimulatorEditor.cpp
@@ -0,0 +1,14 @@
+#include "SistineSimulatorEditor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FSistineSimulatorEditorModule, SistineSimulatorEditor);
+
+void FSistineSimulatorEditorModule::StartupModule()
+{
+    // Editor module startup logic here
+}
+
+void FSistineSimulatorEditorModule::ShutdownModule()
+{
+    // Editor module shutdown logic here
+}
